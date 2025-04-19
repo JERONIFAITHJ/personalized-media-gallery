5674e80e8437f01825fa54b683782e4ce126851a
JERONIFAITHJ <jeronij10@gmail.com>
2025-04-19 18:25:08 +0530

    feat: add google sign in

---
+++ components/GoogleSignIn.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+
+const GoogleSignIn = () => {
+  const [loading, setLoading] = useState(false);
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://accounts.google.com/gsi/client";
+    script.async = true;
+    script.defer = true;
+    script.onload = () => {
+      window.google.accounts.id.initialize({
+        client_id: clientId,
+        callback: handleCredentialResponse,
+      });
+      window.google.accounts.id.renderButton(
+        document.getElementById("google-btn"),
+        { theme: "filled_blue", size: "large", width: "200" }
+      );
+    };
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const handleCredentialResponse = async (response) => {
+    setLoading(true);
+    console.log(response);
+    const credential = response.credential;
+    console.log("CREDENTIALS: ", credential);
+    const resp = await fetch("http://localhost:3003/auth/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ credential }),
+    });
+  };
+
+  return (
+    <div className="">
+      {loading && <p>Loading...</p>}
+      <div id="google-btn"></div>
+    </div>
+  );
+};
+
+export default GoogleSignIn;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import "./App.css";
-import Sort from "../components/Sort";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import GoogleSignIn from "../components/GoogleSignIn";
 
 function App() {
   return (
-    <div className="w-full">
-      <header>My app</header>
+    <div className="w-full min-h-full">
+      {/* <header className="flex justify-end gap-5 p-4 mr-4 ml-4 text-xl">
+        <Link to="/login">Sign in</Link>
+      </header> */}
+      <div className="px-30 py-50 h-100% flex justify-center items-center pb-30">
+        <div className="flex flex-col justify-start gap-10 text-2xl italic">
+          <h1 className="text-indigo-300">
+            Store, manage, and share your files — effortlessly.
+          </h1>
+          <h1 className="text-purple-300">
+            Securely upload and share your files — only with who you choose.
+          </h1>
+          <h1 className="text-pink-300">Upload. View. Share. Done.</h1>
+        </div>
+      </div>
+      <div className="w-full flex justify-center">
+        <GoogleSignIn />
+      </div>
       <Outlet />
     </div>
   );
